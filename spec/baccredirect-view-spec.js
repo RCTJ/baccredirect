b42063732675262d826ac138ff775231982909a5
SBOAAAA <fun_pu@hotmail.com>
2022-11-23 15:47:56 +0700

    edit

---
+++ spec/baccredirect-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BaccredirectView from '../lib/baccredirect-view';
+
+describe('BaccredirectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
